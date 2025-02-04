0db05046d60eb2bad656ba2e0a9e0310d9272c98
mrpunpoon-303 <vaetit@gmail.com>
2025-02-05 00:30:00 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from dash import Dash
+import dash_bootstrap_components as dbc
+from layouts import create_layout
+from callbacks import register_callbacks
+
+# Initialize the Dash app
+app = Dash(__name__, 
+    external_stylesheets=[
+        dbc.themes.BOOTSTRAP,
+        'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css'
+    ],
+    suppress_callback_exceptions=True
+)
+
+# Set up the app layout
+app.layout = create_layout()
+
+# Register callbacks
+register_callbacks(app)
+
+#server = app.server  # For deployment
+#http://127.0.0.1:8062/
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8062)
